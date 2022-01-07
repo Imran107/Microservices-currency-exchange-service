d362dca9eccca5f832d53c1ecaeabe9ad3f4f410
Imran107 <imran.itim@gmail.com>
2022-01-07 21:22:26 +0500

    Service to exchange currency with spring data JPA

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+VALUES(11111, 'USD', 'PKR', 175, '8000');
+INSERT INTO currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+VALUES(11112, 'EUR', 'PKR', 110, '8000');
+INSERT INTO currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+VALUES(11113, 'AUD', 'PKR', 120, '8000');
